4764a8be97f834d4a42668a1a55a1f4417d4017c
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2021-06-13 15:40:59 +0100

    added GET functionality

--- songs/urls.py
+++ songs/urls.py
@@ -1,8 +1,13 @@
+from songs.models import Song
 from django.urls import path
 from django.urls import path
-from .views import SongListView
+from .views import (
+    SongListView,
+    SongDetailView
+    )
 
 
 urlpatterns = [
-    path('', SongListView.as_view())
+    path('', SongListView.as_view()),
+    path('<int:pk>/', SongDetailView.as_view()),
 ]
--- songs/views.py
+++ songs/views.py
@@ -3,14 +3,29 @@ from songs.serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound, PermissionDenied
 
 from .models import Song
 from .serializers import SongSerializer
-# Create your views here.
+
 
 class SongListView(APIView):
 
-    def get(self, request):
+    def get(self, _request):
         songs = Song.objects.all()
         serialized_songs = SongSerializer(songs, many=True)
         return Response(serialized_songs.data, status=status.HTTP_200_OK)
+
+
+class SongDetailView(APIView):
+
+    def get_song(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise NotFound()
+
+    def get(self, _request, pk):
+        song = self.get_song(pk=pk)
+        serialized_song = SongSerializer(song)
+        return Response(serialized_song.data, status=status.HTTP_200_OK)
